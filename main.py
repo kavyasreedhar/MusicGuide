05249161f950e5e852c122da4e089c4d6c60b1c8
Kavya Sreedhar <skavya@stanford.edu>
2020-10-30 20:25:05 -0700

    runs

--- main.py
+++ main.py
@@ -7,24 +7,26 @@ class Note():
               accidental=None):
   	
     self.value = value
-    self.octave = octave
     self.length = length
-    self.accidential = accidental
+    self.is_rest = is_rest
+    self.octave = octave
+    self.accidental = accidental
     
-  def print():
+  def print(self):
     out = ""
     out += self.value
     if self.accidental == "#":
       out += "is"
-    elif self.accidential == "b":
+    elif self.accidental == "b":
     	out += "es"
     # pitch 
-    if self.octave > 4:
-      for i in range(self.octave - 4):
-        out += "'"  
-    elif self.octave < 4:
-      for i in range(4 - self.octave):
-      	out += ","  
+    if not self.is_rest:
+        if self.octave > 4:
+          for i in range(self.octave - 4):
+            out += "'"  
+        elif self.octave < 4:
+          for i in range(4 - self.octave):
+      	    out += ","  
     
     out += self.length
     return out
@@ -33,8 +35,8 @@ class Score():
   def __init__(self):
     self.notes = []
     
-  def add_notes(notes):
-  	for note in notes:
+  def add_notes(self, notes):
+    for note in notes:
       note_parts = note.split(".")
       note_val, note_len = note_parts[0], note_parts[-1]
       is_rest = (note_val == "R")
@@ -50,9 +52,9 @@ class Score():
                              length=note_len,
                              is_rest=is_rest,
                              octave=note_octave,
-                             accidential=accidental))
+                             accidental=accidental))
     
-  def generate_lilypond(filename):
+  def generate_lilypond(self, filename):
     with open(filename, "w+") as output:
       output.write("\relative c' {\n")
       
@@ -61,6 +63,6 @@ class Score():
         output.write(" ")
       output.write("}\n")
     
-s = Score()
+score = Score()
 score.add_notes(["C#.4.16", "Eb.4.2", "G.4.4", "E.4.8", "R.4"])
 score.generate_lilypond("t.txt")
