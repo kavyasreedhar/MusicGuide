e714a2d2a9e54f91a480b4924889fff2032eee98
Sophia Liu <sophliu@pono1.stanford.edu>
2020-11-07 18:01:22 -0800

    lyrics

--- birthday.py
+++ birthday.py
@@ -13,7 +13,7 @@ notes += ["G.4.8", "G.4.8", "A.4.4", "G.4.4", "D.5.4", "C.5.2"]
 notes += ["G.4.8", "G.4.8", "G.5.4", "E.5.4", "C.5.4", "B.4.4", "A.4.4"]
 notes += ["F.5.8", "F.5.8", "E.5.4", "C.5.4", "D.5.4", "C.5.2", "R.4"]
 score.add_notes(notes)
-score.add_lyrics("birth -- day to you! Hap -- py birth -- day to you! Hap -- py birth -- day dear Soph -- ia, Hap -- py birth -- day to you!")
+score.add_lyrics("Hap -- py birth -- day to you! Hap -- py birth -- day to you! Hap -- py birth -- day dear Soph -- ia, Hap -- py birth -- day to you!")
 score.generate_lilypond("birthday.ly")
 
 
