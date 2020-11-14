8b3a533fadbe1d3afc6b1d6dec726a9be7356fe1
Kavya Sreedhar <skavya@stanford.edu>
2020-11-14 08:12:57 -0800

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+        name='MusicGuide',
+        packages=[
+            "MusicGuide"
+        ],
+        version='0.0.1',
+        url="https://github.com/kavyasreedhar/MusicGuide",
+        python_requires=">=3.5",
+)
