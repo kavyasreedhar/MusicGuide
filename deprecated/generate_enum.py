7b3d3dbea94ddae1ba2d0574ebc4330c372ab303
Kavya Sreedhar <skavya@stanford.edu>
2020-11-14 08:01:26 -0800

    delete deprecated

--- deprecated/generate_enum.py
+++
@@ -1,8 +0,0 @@
-print("class NoteValue(IntEnum):")
-i = 0
-for letter in ["A", "B", "C", "D", "E", "F", "G", "a", "b", "c", "d", "e", "f", "g"]:
-    for accidential in ["s", "f", ""]:
-        for octave in range(8):
-            print("  " + letter + accidential + str(octave) + " = " + str(i))
-            i += 1
-
